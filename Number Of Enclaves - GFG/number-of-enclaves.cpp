3a7318235f8c0862358a75fd67d71b11823dd2db
Ishanya Yadav <71209292+aynahsi@users.noreply.github.com>
2023-07-30 23:39:16 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -10,59 +10,50 @@ using namespace std;
 
 class Solution {
   public:
+  
+    void solve( int row ,int col ,vector<vector<int>> &grid,int n,int m, vector<vector<int>>&vis ){
+        vis[row][col] = 1;
+        
+        int drow[] = {-1,0,1,0};
+        int dcol[] ={0,1,0,-1} ;
+        
+        for(int k = 0 ; k < 4; k++){
+        int nrow = row + drow[k] ;
+        int ncol = col + dcol[k] ;
+        
+        if (nrow >=0 && nrow<n && ncol>=0 && ncol <m && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
+             
+             solve(nrow,ncol, grid,n,m,vis);
+        }
+        
+    }
+    }
     int numberOfEnclaves(vector<vector<int>> &grid) {
         // Code here
-        queue<pair<int,int>>q;
-	    
-	     // Code 
-	     int n = grid.size();
-	     int m = grid[0].size() ;
-	     int count = 0 ;
-	    vector<vector<int>>vis(n,vector<int>(m,0)) ;
-	    for(int i = 0;i< n ; i++){
-	        for ( int j = 0;j<m ; j++){
-	            if (i == 0 || j == 0 || i == n-1 || j == m-1){
-	                if (grid[i][j] == 1){
-	                q.push({i,j}) ;
-	                vis[i][j] = 1;
-	            }
-	            }
-	        }
-	    }
-	    
-	     
-	    
-	        while (!q.empty()){
-	        int r = q.front().first;
-	        int c = q.front().second;
-	       
-	        
-	        q.pop() ;
-	       
-	       
-	        int delrow[] = {-1,0,1,0};//for movement
-	        int delcol[] = {0,1,0,-1 } ;
-	        for ( int i = 0; i <4 ;i++){
-	        int nrow = r + delrow[i] ;
-	        int ncol = c + delcol[i] ;
-	        if (nrow>= 0 && nrow <n && ncol>=0 && ncol<m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1 ){
-	            q.push({nrow,ncol});
-	            vis[nrow][ncol] = 1;
-	        }
-	    }
-    }
-    
-    
-    
-    for ( int i = 0; i<n; i++){
-        for ( int j = 0 ;j<m ;j++){
-            if(vis[i][j] == 0 && grid[i][j] ==1){
-                count++;
+        int n = grid.size() ;
+        int m = grid[0].size() ;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j < m ; j++){
+                if ( i == 0 || j == 0 || i == n-1 || j == m-1){
+                    if (grid[i][j] == 1 && vis[i][j] == 0){
+                        solve(i, j, grid,n,m, vis);
+                    }
+                }
             }
         }
-    }
-    
-    return count;
+        
+        int count = 0 ;
+        for (int i = 0 ; i < n ;i++){
+            for (int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 1 && vis[i][j] == 0){
+                    count++;
+                }
+            }
+        }
+        
+        return count ;
     }
 };
 
